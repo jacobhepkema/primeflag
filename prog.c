c42611db7031052dc7949d18f21842dfc66e81bf
Jacob Hepkema <41913625+jacobhepkema@users.noreply.github.com>
2021-09-24 17:56:46 +0100

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int main( int b,
+     char**i){int p=0
+      ;for(int z=1; z<
+       atoi(i[1]);z++){
+      p=0;for(int j=2;
+     j<z;j++){if(z %j
+    ==0){p=1;}}if( p
+   == 0 ) { printf(
+   "%d\n",z ) ; } }
+     return  0  ;   }
